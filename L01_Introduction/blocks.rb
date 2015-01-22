16081a0355ea542cf22dc3d71b24808975a2ebc9
broflovski <crixalis896@gmail.com>
2015-01-22 14:50:56 +0800

    init commit

--- L01_Introduction/blocks.rb
+++ L01_Introduction/blocks.rb
@@ -1,6 +1,12 @@
 class Blocks
   def traversing
     1.upto(9){|x| puts x}
+    puts #puts自动换行 print不自动换行
+    a=[3,2,1]
+    a[3]=a[2]-1
+    a.each do |elt|
+      puts elt+1
+    end
   end
 end
 
